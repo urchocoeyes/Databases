f1f25ba1efa2af434063d45820ee74526c0aab90
Nazym <nazymtolebek@gmail.com>
2024-06-13 22:25:54 +0500

    ssql

--- lecture1.sql
+++ lecture1.sql
@@ -5,13 +5,13 @@ create table instructor(
 	name varchar(20),
 	dept_name varchar(20),
 	salary numeric(8, 2) -- Total digits (Precision): 8 , Decimal places (Scale): 2, can: 123456.78 (6 digits before, 2 digits after)
-)
+);
 
 create table corporation(
 	corp_id smallint, -- 2 bytes, Range: -32,768 to 32,767
 	name varchar(30),
 	constraint pk_corporation primary key (corp_id)
-)
+);
 
 
 -- SMALLINT: 2 bytes = 16 bits, 1 bit for sign, 15 bits for value: 2 * 15 = 32768
@@ -20,3 +20,6 @@ create table corporation(
 -- BIGINT: 8 bytes, range from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
 
 
+insert into corporation (corp_id, name) values (27, 'A P Corp');
+
+select name from corporation where corp_id=27;
