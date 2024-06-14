2d5b91a10ce5722626952a78d3de98dc54bded1b
Nazym <nazymtolebek@gmail.com>
2024-06-14 16:52:26 +0500

    s

--- lecture2.sql
+++ lecture2.sql
@@ -35,8 +35,49 @@ create table distributors(
 create table products (
 	product_no integer,
 	name text,
-	price numeric default 7.77
+	price numeric default 7.77 -- numeric(precision, scale) here unlimited as not specified
 );
+
+alter table distributors add column address varchar(30);
+
+alter table distributors drop column address restrict;
+
+alter table distributors alter column address type varchar(80), alter column name type varchar(100);
+
+alter table distributors alter column address set data type varchar(80), alter column name set data type varchar(100);
+
+drop table films, distributors cascade; -- dependent etc also delete
+
+-- INTEGERs
+-- smallint(2 bytes) = (-32768 to + 32768)
+-- integer(4 bytes) = (-2147483648 to + 2147....)
+-- bigint(8 bytes) = (-9,223,372,036,854,775,808 to +9,223,372,036,854,775,807)
+
+-- FLOATs
+-- real(4 bytes)
+-- double(8 bytes)
+
+-- Auto incrementing types
+-- smallserial (2 bytes) - small auto incrementing integer
+-- serial(4 bytes) - autoincr int
+
+-- character types
+-- varchar(n)
+-- char(n)
+-- text
+
+-- BINARY
+-- bytea (1 to 4 types)
+
+-- TIME/ DATE
+-- timestamp with time zone -- date, time, zone
+-- timestamp without time zone -- date, time
+-- date -- date without time
+-- interval - time interval
+
+-- boolean
+
+
 
 
 
